3ccf0a6873a86a72523ece030fdfab34c96c967b
Stefano Cocchi <stefano.cocchi.it@gmail.com>
2018-11-14 16:41:33 +0000

    Added npm package installation when building angular frontend

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ SET mypath=%~dp0
 dotnet restore TungstenRssReader.sln
 dotnet build --configuration Release --no-restore
 dotnet test --configuration Release --no-build --no-restore TungstenRssReader.UnitTests\TungstenRssReader.UnitTests.csproj
-cmd /c "pushd %mypath:~0,-1%\TungstenRssReader\AngularClientApp && ng build --prod && popd"
+cmd /c "pushd %mypath:~0,-1%\TungstenRssReader\AngularClientApp && npm install && ng build --prod && popd"
 dotnet publish --configuration Release --no-build --no-restore --output %mypath:~0,-1%\build TungstenRssReader\TungstenRssReader.csproj
 echo Build OK
